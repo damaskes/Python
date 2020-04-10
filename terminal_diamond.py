eadc0df5f5308f5c56bc321a26969a66a489d3d3
Ihskhan Verdyan (Ишхан Вердян) <vermasstud@gmail.com>
2020-04-10 15:25:04 +0300

    Create terminal_diamond.py

---
+++ terminal_diamond.py
@@ -0,0 +1,32 @@
+def wait_for_input():
+    try:
+        steps = int(input('Insert digit from 1 to 9: '))
+    except ValueError:
+        print('Incorrect character')
+        wait_for_input()
+    else:
+        return steps
+
+
+def make_diamond(steps):
+    """
+    Creates diamond brick in console
+    """
+    columns = []
+    arr = list(range(1, steps + 1)) + list(range(steps - 1, 0, -1))
+    for i in arr:
+        rows = []
+        dig = i - steps
+        for j in arr:
+            res = j + dig
+            if res < 1:
+                res = " "
+            rows.append(res)
+        columns.append(rows)
+    for col in columns:
+        print(*col)
+
+
+if __name__ == '__main__':
+    make_diamond(wait_for_input())
+
